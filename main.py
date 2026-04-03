cb69b9d5807b7833a45d101615a6022e4665547c
paulviville <paul.viville@gmail.com>
2026-04-04 01:22:17 +0200

    wip testing core and types

--- main.py
+++ main.py
@@ -3,17 +3,28 @@ import websockets
 import uuid
 import json
 
-# from SyncModulesPython.Core.ModuleCore import ModuleCore
+from SyncModulesPython.Core.ModuleCore import ModuleCore
 
 from SyncModulesPython.Core.ModulesRegistry import ModulesRegistry
-# import SyncModulesPython as Modules
-print
-module = ModulesRegistry( 1234 )
+
+
+# registry.onCommand("ADD_MODULE", {"type":"ModuleCore", "UUID":str(uuid.uuid4())})
+
+# def onModuleMessage ( payload ):
+# 	moduleUUID = uuid.UUID(payload[ "moduleUUID" ])
+# 	module = registry.getModule( moduleUUID )
+# 	print( moduleUUID, module )
+# 	if module is not None:
+# 		module.input( payload )
 
 class ClientNetwork:
 	def __init__( self ):
 		print( "ClientNetwork - __init__" )
 		self.websocket = None
+
+	async def run( self ):
+		while True:
+			await self.listen
 
 	async def connect ( self, url="ws://localhost", port="3000" ):
 		print( f'{url}:{port}')
@@ -30,6 +41,14 @@ class ClientNetwork:
 
 	def onMessage ( self, message ):
 		print(f'received: { message }')
+		messageData = json.loads( message )
+		scope = messageData[ "scope" ]
+		payload = messageData[ "payload" ]
+		match scope:
+			case "SYSTEM":
+				print("system message")
+			case "MODULE":
+				self.onModuleMessage( payload )
 
 	async def listen ( self ):
 		try:
@@ -43,19 +62,71 @@ class ClientNetwork:
 			await self.websocket.close( )
 			print( "disconnected" )
 
+	def setCallbacks ( self, onModuleMessage ):
+		self.onModuleMessage = onModuleMessage
+
 # await clientNetwork.connect()
+UUID = uuid.uuid4( )
 
 async def main( ):
 	clientNetwork = ClientNetwork( )
+		
+	def outputFn ( payload ):
+		message = {
+			"scope": "MODULE",
+			"senderUUID": str(UUID),
+			"payload": payload
+		}
+		print( message )
+		clientNetwork.sendFn( json.dumps( message ) )
+
+	registry = ModulesRegistry( outputFn )
+
+	def onModuleMessage ( payload ):
+		moduleUUID = uuid.UUID(payload[ "moduleUUID" ])
+		module = registry.getModule( moduleUUID )
+		print( "onMOduleMessage", moduleUUID, module )
+		if module is not None:
+			module.input( payload )
+
+	clientNetwork.setCallbacks( onModuleMessage )
+
+
 	await clientNetwork.connect( )
 	asyncio.create_task( clientNetwork.listen( ) )
+	# asyncio.run(clientNetwork.run())
+	print( uuid.UUID(int=0))
 
 	message = {
-		"UUID": str( uuid.uuid4( ) )
+		"UUID": str( UUID )
 	}
 	clientNetwork.sendFn( json.dumps( message ) )
 	# await clientNetwork.send( json.dumps( message ) )
+	await asyncio.sleep(0.5)
+	
+	message = {
+		"scope": "SYSTEM",
+		"senderUUID": str( UUID ),
+		"payload": {
+			"command": "INSTANCE_JOIN",
+			"data": {
+				"instanceUUID": str( uuid.UUID(int=0)),
+				"userUUID": str( UUID )
+			}
+		}
+	}
+	clientNetwork.sendFn( json.dumps( message ) )
+	await asyncio.sleep(0.5)
 
-	await asyncio.sleep(30)
+	scalar = registry.addModule( "Vector3Module", uuid.UUID(int=1), True)
+	await asyncio.sleep(0.5)
+	scalar.updateVector( [1, 1, 2], True )
+	await asyncio.sleep(0.5)
+	registry.removeModule( uuid.UUID(int=1), True )
+	
+	print( registry.modulesList())
+
+	while True:
+		await asyncio.sleep(1)
 
 asyncio.run( main( ) )
